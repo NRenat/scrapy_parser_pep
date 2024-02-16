c13138e7a780a39e372e36506b61b1f7798b3c0b
NRenat <nrenat1606@yandex.ru>
2024-02-16 15:09:39 +0300

    Some changes

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,11 +1,18 @@
 from collections import Counter
 from datetime import datetime
 
+from pep_parse.settings import BASE_DIR, DATETIME_FORMAT, FILE_FORMAT, RESULTS
+
 
 class PepParsePipeline:
     def __init__(self):
         self.status_counter = Counter()
         self.total_statuses = 0
+        self.results_dir = BASE_DIR / RESULTS
+        self.results_dir.mkdir(exist_ok=True)
+
+    def open_spider(self, spider):
+        pass
 
     def process_item(self, item, spider):
         status = item.get('status')
@@ -15,8 +22,9 @@ class PepParsePipeline:
         return item
 
     def close_spider(self, spider):
-        current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        filename = f'results/status_summary_{current_time}.csv'
+        current_time = datetime.now().strftime(DATETIME_FORMAT)
+        filename = (f'{self.results_dir}'
+                    f'/status_summary_{current_time}.{FILE_FORMAT}')
 
         with open(filename, 'w', encoding='utf-8') as f:
             f.write('Статус,Количество\n')
--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 BOT_NAME = "pep_parse"
 
 SPIDER_MODULES = ["pep_parse.spiders"]
@@ -5,16 +7,25 @@ NEWSPIDER_MODULE = "pep_parse.spiders"
 
 ROBOTSTXT_OBEY = True
 
+BASE_DIR = Path(__file__).parent.parent
+
+RESULTS = 'results'
+RESULTS_DIR = BASE_DIR / RESULTS
+
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+FILE_FORMAT = 'csv'
 
 FEEDS = {
-    'results/pep_%(time)s.csv': {
-        'format': 'csv'
+    f'{RESULTS}/pep_%(time)s.csv': {
+        'format': FILE_FORMAT,
+        'fields': ('number', 'name', 'status')
     }
 }
 
 ITEM_PIPELINES = {
-    'pep_parse.pipelines.PepParsePipeline': 100,
+    'pep_parse.pipelines.PepParsePipeline': 300,
 }
