f85a1a44aa392f50d04264bab88c6c0ed23126d0
NRenat <nrenat1606@yandex.ru>
2024-02-17 12:11:37 +0300

    small review changes

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,3 +1,4 @@
+import csv
 from collections import Counter
 from datetime import datetime
 
@@ -6,28 +7,33 @@ from pep_parse.settings import BASE_DIR, DATETIME_FORMAT, FILE_FORMAT, RESULTS
 
 class PepParsePipeline:
     def __init__(self):
-        self.status_counter = Counter()
-        self.total_statuses = 0
-        self.results_dir = BASE_DIR / RESULTS
-        self.results_dir.mkdir(exist_ok=True)
+        self.total_statuses = None
+        self.status_counter = None
 
     def open_spider(self, spider):
-        pass
+        self.status_counter = Counter()
+        self.total_statuses = 0
 
     def process_item(self, item, spider):
         status = item.get('status')
         if status:
             self.status_counter[status] += 1
-            self.total_statuses += 1
         return item
 
     def close_spider(self, spider):
+        results_dir = BASE_DIR / RESULTS
+        results_dir.mkdir(exist_ok=True)
         current_time = datetime.now().strftime(DATETIME_FORMAT)
-        filename = (f'{self.results_dir}'
+        filename = (f'{results_dir}'
                     f'/status_summary_{current_time}.{FILE_FORMAT}')
 
-        with open(filename, 'w', encoding='utf-8') as f:
-            f.write('Статус,Количество\n')
+        self.total_statuses = sum(self.status_counter.values())
+
+        with open(filename, 'w', encoding='utf-8', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['Статус', 'Количество'])
+
             for status, count in self.status_counter.items():
-                f.write(f'{status}, {count}\n')
-            f.write(f'Total,{str(self.total_statuses)}\n')
+                writer.writerow([status, count])
+
+            writer.writerow(['Total', str(self.total_statuses)])
